8a67b2217624656a08d76a01bd0cee7323175cbf
Richard Penner <richardpenner@gmail.com>
2025-12-09 12:36:36 -0800

    Refactor doc of Polish notation to one location.

--- src/logic/mod.rs
+++ src/logic/mod.rs
@@ -38,36 +38,13 @@ pub fn require_var_is_boolean<V: Metavariable>(some_var: &V) -> Result<(), MguEr
 
 /// Take a string in Polish notation and return the corresponding Statement.
 ///
-/// Operators of arity 1 and 2 as tabulated by Prior following Bochenski and Łukasiewicz:
-///
-/// | Table        | Łukasiewicz | [`BooleanSimpleOp`] | Table        | Łukasiewicz | [`BooleanSimpleOp`] |
-/// | ------------ | ----------- | ------------------- | ------------ | ----------- | ------------------- |
-/// |              |             |                     | (0, 1)       | N           | [`NotA1`]           |
-/// | (1, 1, 1, 0) | A           | [`OrAB2`]           | (0, 0, 0, 1) | X           | [`NotOrAB2`]        |
-/// | (1, 1, 0, 1) | B           | [`ImpliesBA2`]      | (0, 0, 1, 0) | M           | [`NotImpliesBA2`]   |
-/// | (1, 0, 1, 1) | C           | [`ImpliesAB2`]      | (0, 1, 0, 0) | L           | [`NotImpliesAB2`]   |
-/// | (0, 1, 1, 1) | D           | [`NotAndAB2`]       | (1, 0, 0, 0) | K           | [`AndAB2`]          |
-/// | (1, 0, 0, 1) | E           | [`BiimpAB2`]        | (0, 1, 1, 0) | J           | [`XorAB2`]          |
-/// | (0, 0, 1, 1) | F           | [`NotA2`]           | (1, 1, 0, 0) | I           | [`IdA2`]            |
-/// | (0, 1, 0, 1) | G           | [`NotB2`]           | (1, 0, 1, 0) | H           | [`IdB2`]            |
-/// | (0, 0, 0, 0) | O           | [`False2`]          | (1, 1, 1, 1) | V           | [`True2`]           |
-///
-/// Operators of arity 0 and 3 novel to this function:
-///
-/// | Additional | [`BooleanSimpleOp`] | Additional | [`BooleanSimpleOp`] |
-/// | ---------- | ------------------- | ---------- | ------------------- |
-/// | 0          | [`False0`]          | 1          | [`True0`]           |
-/// | &amp;      | [`And3ABC3`]        | \|         | [`Or3ABC3`]         |
-/// | +          | [`Xor3ABC3`]        | ^          | [`Majority3ABC3`]   |
-/// | ?          | [`IfABC3`]          | ;          | term separator      |
+/// # Syntax
 ///
 /// Lowercase letters correspond to the supplied Boolean variables.
 /// Whitespace and control characters are ignored.
+/// Semicolons (;) are used as term separators.
 ///
-/// Adopted from:
-///
-/// **Prior, A. N.** (1955) *Formal Logic*, pp. 4-13.
-/// Available at <https://archive.org/details/formallogic0000anpr>
+#[cfg_attr(doc, doc = include_str!("LUKASIEWICZ_NOTATION.md"))]
 ///
 /// # Errors
 ///
@@ -77,32 +54,6 @@ pub fn require_var_is_boolean<V: Metavariable>(some_var: &V) -> Result<(), MguEr
 /// - Unknown characters appear in the notation
 /// - Stack underflow/overflow occurs during parsing
 /// - No terms are specified
-///
-/// [`BooleanSimpleOp`]: `crate::bool_eval::BooleanSimpleOp`
-/// [`NotA1`]: `crate::bool_eval::BooleanSimpleOp::NotA1`
-/// [`False2`]: `crate::bool_eval::BooleanSimpleOp::False2`
-/// [`NotOrAB2`]: `crate::bool_eval::BooleanSimpleOp::NotOrAB2`
-/// [`NotImpliesAB2`]: `crate::bool_eval::BooleanSimpleOp::NotImpliesAB2`
-/// [`NotB2`]: `crate::bool_eval::BooleanSimpleOp::NotB2`
-/// [`NotImpliesBA2`]: `crate::bool_eval::BooleanSimpleOp::NotImpliesBA2`
-/// [`NotA2`]: `crate::bool_eval::BooleanSimpleOp::NotA2`
-/// [`XorAB2`]: `crate::bool_eval::BooleanSimpleOp::XorAB2`
-/// [`NotAndAB2`]: `crate::bool_eval::BooleanSimpleOp::NotAndAB2`
-/// [`AndAB2`]: `crate::bool_eval::BooleanSimpleOp::AndAB2`
-/// [`BiimpAB2`]: `crate::bool_eval::BooleanSimpleOp::BiimpAB2`
-/// [`IdA2`]: `crate::bool_eval::BooleanSimpleOp::IdA2`
-/// [`ImpliesBA2`]: `crate::bool_eval::BooleanSimpleOp::ImpliesBA2`
-/// [`IdB2`]: `crate::bool_eval::BooleanSimpleOp::IdB2`
-/// [`ImpliesAB2`]: `crate::bool_eval::BooleanSimpleOp::ImpliesAB2`
-/// [`OrAB2`]: `crate::bool_eval::BooleanSimpleOp::OrAB2`
-/// [`True2`]: `crate::bool_eval::BooleanSimpleOp::True2`
-/// [`False0`]: `crate::bool_eval::BooleanSimpleOp::False0`
-/// [`True0`]: `crate::bool_eval::BooleanSimpleOp::True0`
-/// [`And3ABC3`]: `crate::bool_eval::BooleanSimpleOp::And3ABC3`
-/// [`Or3ABC3`]: `crate::bool_eval::BooleanSimpleOp::Or3ABC3`
-/// [`Xor3ABC3`]: `crate::bool_eval::BooleanSimpleOp::Xor3ABC3`
-/// [`Majority3ABC3`]: `crate::bool_eval::BooleanSimpleOp::Majority3ABC3`
-/// [`IfABC3`]: `crate::bool_eval::BooleanSimpleOp::IfABC3`
 pub fn build_boolean_statement_from_polish<Ty, V, N, T, TF>(
     polish: &str,
     factory: &TF,
--- src/logic/propositional/axioms/cn_basis/mod.rs
+++ src/logic/propositional/axioms/cn_basis/mod.rs
@@ -1,51 +1,17 @@
 //! Axioms of Propositional Calculus in the C-N basis.
 //!
-//! "C" here refers to material implication and "N" to the not operator.
+//! "C" here refers to material implication and "N" to the not operator
+//! in a syntax for propositional logic due to Łukasiewicz.
 //!
-//! As tabulated by Prior following Bochenski and Łukasiewicz:
+#![cfg_attr(doc, doc = include_str!("../../../LUKASIEWICZ_NOTATION.md"))]
 //!
-//! | Table        | Łukasiewicz | [`BooleanSimpleOp`] | Table        | Łukasiewicz | [`BooleanSimpleOp`] |
-//! | ------------ | ----------- | ------------------- | ------------ | ----------- | ------------------- |
-//! | (1, 0)       |             | [`IdA1`]            | (0, 1)       | N           | [`NotA1`]           |
-//! | (1, 1, 1, 0) | A           | [`OrAB2`]           | (0, 0, 0, 1) | X           | [`NotOrAB2`]        |
-//! | (1, 1, 0, 1) | B           | [`ImpliesBA2`]      | (0, 0, 1, 0) | M           | [`NotImpliesBA2`]   |
-//! | (1, 0, 1, 1) | C           | [`ImpliesAB2`]      | (0, 1, 0, 0) | L           | [`NotImpliesAB2`]   |
-//! | (0, 1, 1, 1) | D           | [`NotAndAB2`]       | (1, 0, 0, 0) | K           | [`AndAB2`]          |
-//! | (1, 0, 0, 1) | E           | [`BiimpAB2`]        | (0, 1, 1, 0) | J           | [`XorAB2`]          |
-//! | (0, 0, 1, 1) | F           | [`NotA2`]           | (1, 1, 0, 0) | I           | [`IdA2`]            |
-//! | (0, 1, 0, 1) | G           | [`NotB2`]           | (1, 0, 1, 0) | H           | [`IdB2`]            |
-//! | (0, 0, 0, 0) | O           | [`False2`]          | (1, 1, 1, 1) | V           | [`True2`]           |
+//! The list of the C-N axiom systems came from:
+//! **Prior, A. N.** (1955) *Formal Logic*, pp. 301-303.
 //!
 //! - Metamath standard following Łukasiewicz: [`simp()`], [`frege()`], and [`transp()`].
 //! - Frege's system (1879): [`simp()`], [`frege()`], [`pm2_04()`], [`pm2_16()`], [`pm2_12()`], and [`pm2_14()`].
 //! - Łukasiewicz's Standard C-N System (1929): [`pm2_06()`], [`pm2_18()`], and [`pm2_24()`]
 //! - Meredith's Single-Axiom C-N System (1953): [`meredith_cn()`]
-//!
-//!
-//! Adopted from:
-//!
-//! **Prior, A. N.** (1955) *Formal Logic*, pp. 4-13, 301-303.
-//! Available at <https://archive.org/details/formallogic0000anpr>
-//!
-//! [`BooleanSimpleOp`]: `crate::bool_eval::BooleanSimpleOp`
-//! [`IdA1`]: `crate::bool_eval::BooleanSimpleOp::IdA1`
-//! [`NotA1`]: `crate::bool_eval::BooleanSimpleOp::NotA1`
-//! [`False2`]: `crate::bool_eval::BooleanSimpleOp::False2`
-//! [`NotOrAB2`]: `crate::bool_eval::BooleanSimpleOp::NotOrAB2`
-//! [`NotImpliesAB2`]: `crate::bool_eval::BooleanSimpleOp::NotImpliesAB2`
-//! [`NotB2`]: `crate::bool_eval::BooleanSimpleOp::NotB2`
-//! [`NotImpliesBA2`]: `crate::bool_eval::BooleanSimpleOp::NotImpliesBA2`
-//! [`NotA2`]: `crate::bool_eval::BooleanSimpleOp::NotA2`
-//! [`XorAB2`]: `crate::bool_eval::BooleanSimpleOp::XorAB2`
-//! [`NotAndAB2`]: `crate::bool_eval::BooleanSimpleOp::NotAndAB2`
-//! [`AndAB2`]: `crate::bool_eval::BooleanSimpleOp::AndAB2`
-//! [`BiimpAB2`]: `crate::bool_eval::BooleanSimpleOp::BiimpAB2`
-//! [`IdA2`]: `crate::bool_eval::BooleanSimpleOp::IdA2`
-//! [`ImpliesBA2`]: `crate::bool_eval::BooleanSimpleOp::ImpliesBA2`
-//! [`IdB2`]: `crate::bool_eval::BooleanSimpleOp::IdB2`
-//! [`ImpliesAB2`]: `crate::bool_eval::BooleanSimpleOp::ImpliesAB2`
-//! [`OrAB2`]: `crate::bool_eval::BooleanSimpleOp::OrAB2`
-//! [`True2`]: `crate::bool_eval::BooleanSimpleOp::True2`
 
 use crate::logic::require_var_is_boolean;
 use crate::{Metavariable, MguError, Node, Statement, Term, TermFactory, Type};
